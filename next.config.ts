5b994c01a3415a00ff2dc24dbc5e8b60a4c7e479
Tammo Feldmann <tammo@developmentseed.org>
2024-11-03 12:43:46 -0500

    Update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // Disable image optimization, necessary for GitHub Pages
+  },
 };
-
 export default nextConfig;
