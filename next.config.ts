a30e29e4466820d49520b0bf8fec09a7def428f1
Tammo Feldmann <tammo@developmentseed.org>
2024-11-03 13:08:00 -0500

    Update base path config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  distDir: "out",
   images: {
     unoptimized: true, // Disable image optimization, necessary for GitHub Pages
   },
+  basePath: "/jamie-schoen",
 };
 export default nextConfig;
